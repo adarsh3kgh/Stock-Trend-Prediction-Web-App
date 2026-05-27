39cab9972316d4ac5cd3dbfc1f937b32a2e4ee9c
ADARSH KUMAR <132445891+adarsh3kgh@users.noreply.github.com>
2026-05-27 20:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
